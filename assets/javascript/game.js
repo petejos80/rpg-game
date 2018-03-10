dadd0c612cec275d2e5f6dbb6768d5d34d4ab501
Joe Peterson <petejos80@gmail.com>
2018-03-09 23:35:10 -0600

    Updated logic in js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,10 @@
 
 $(document).ready(function(){
 
-
 // VARIABLES
 // =======================================================================================================
 var targetNumber = getRandomArbitrary(20, 100);
-var buttonNumberOptions = [10, 5, 3, 7]; 
-var imageJewel = $("<img>");
+var crystalNumberOptions = [10, 5, 3, 7]; 
 var counter = 0; 
 
 // FUNCTIONS 
@@ -19,9 +17,52 @@ function getRandomArbitrary(min, max) {
 // MAIN PROCESS 
 // =======================================================================================================
 
-//Assign a random number to Number to Guess field
-$("#numberToGuess").text(targetNumber);
+// Assign a random number to Number to Guess field
+$('#numberToGuess').text(targetNumber);
+
+ // Shuffle the array to randomize order
+crystalNumberOptions.sort(function(a, b){return 0.5 - Math.random()});
+
+console.log(crystalNumberOptions);
+
+// Test - remove
+$('#crystal1').on('click', function() {
+    alert('hello')
+});
+
+// $("#crystal1, #crystal2, #crystal3, #crystal4").val(crystalNumberOptions[0,1,2,3]);
+
+$('#crystal1').val(crystalNumberOptions[0]);
+$('#crystal2').val(crystalNumberOptions[1]);
+$('#crystal3').val(crystalNumberOptions[2]);
+$('#crystal4').val(crystalNumberOptions[3]);
+
+//Testing
+var testing1 = $("#crystal1").val();
+console.log('Testing' + testing1);
+
+var testing2 = $("#crystal2").val();
+console.log('Testing' + testing2);
+
+var testing3 = $("#crystal3").val();
+console.log('Testing' + testing3);
+
+var testing4 = $("#crystal4").val();
+console.log('Testing' + testing4);
 
 
+
+
+
+
+
+
+
+
+// for (var i = 0; i < crystalNumberOptions.length; i++) {
+
+//     var imageJewel = $('<img>');
+
+//     }   
 
 });
