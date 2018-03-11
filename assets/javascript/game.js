6fcaf5259afb589c1dd0ba9f0d3f665442935962
Joe Peterson <petejos80@gmail.com>
2018-03-11 00:22:01 -0600

    Finished player score counter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,41 +14,86 @@ function getRandomArbitrary(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
   }
 
+function addToScore() {
+
+}
+
 // MAIN PROCESS 
 // =======================================================================================================
 
 // Assign a random number to Number to Guess field
 $('#numberToGuess').text(targetNumber);
 
+// Assign starting value of 0 to player's  total score
+$('#yourScore').text(counter);
+
  // Shuffle the array to randomize order
-crystalNumberOptions.sort(function(a, b){return 0.5 - Math.random()});
+ crystalNumberOptions.sort(function(a, b){return 0.5 - Math.random()});
 
 console.log(crystalNumberOptions);
 
 // Test - remove
-$('#crystal1').on('click', function() {
-    alert('hello')
-});
+// $('#crystal1').on('click', function() {
+//     counter = counter + 
+// });
 
 // $("#crystal1, #crystal2, #crystal3, #crystal4").val(crystalNumberOptions[0,1,2,3]);
+// $('#crystal1').val(crystalNumberOptions[0]);
+// $('#crystal2').val(crystalNumberOptions[1]);
+// $('#crystal3').val(crystalNumberOptions[2]);
+// $('#crystal4').val(crystalNumberOptions[3]);
 
-$('#crystal1').val(crystalNumberOptions[0]);
-$('#crystal2').val(crystalNumberOptions[1]);
-$('#crystal3').val(crystalNumberOptions[2]);
-$('#crystal4').val(crystalNumberOptions[3]);
+// Assign crystal array value to a new variable
+var crystalValue1 = crystalNumberOptions[0];
+console.log(crystalValue1 + ' is Cystal 1"s Value');
+var crystalValue2 = crystalNumberOptions[1];
+console.log(crystalValue2 + ' is Cystal 2"s Value');
+var crystalValue3 = crystalNumberOptions[2];
+console.log(crystalValue3 + ' is Cystal 3"s Value');
+var crystalValue4 = crystalNumberOptions[3];
+console.log(crystalValue4 + ' is Cystal 4"s Value');
+
+
+// Configured crystals to increase total counter value when clicked
+$('#crystal1').on('click', function() {
+    counter += crystalValue1;
+    $('#yourScore').text(counter);
+});
+
+$('#crystal2').on('click', function() {
+    counter += crystalValue2;
+    $('#yourScore').text(counter);
+});
+
+$('#crystal3').on('click', function() {
+    counter += crystalValue3;
+    $('#yourScore').text(counter);
+});
+
+$('#crystal4').on('click', function() {
+    counter += crystalValue4;
+    $('#yourScore').text(counter);
+});
+
+
+
 
 //Testing
-var testing1 = $("#crystal1").val();
-console.log('Testing' + testing1);
+// var testing1 = $("#crystal1").val();
+// console.log('Testing' + testing1);
 
-var testing2 = $("#crystal2").val();
-console.log('Testing' + testing2);
+// var testing2 = $("#crystal2").val();
+// console.log('Testing' + testing2);
 
-var testing3 = $("#crystal3").val();
-console.log('Testing' + testing3);
+// var testing3 = $("#crystal3").val();
+// console.log('Testing' + testing3);
 
-var testing4 = $("#crystal4").val();
-console.log('Testing' + testing4);
+// var testing4 = $("#crystal4").val();
+// console.log('Testing' + testing4);
+
+// $('#crstal1').on('click')
+
+// console.log(crystalNumberOptions);
 
 
 
